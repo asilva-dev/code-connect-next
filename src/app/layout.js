75e27d995bb3bcdb9ea57a3aa3421ce15e898d1c
asilva-dev <amanda.adgti@gmail.com>
2024-09-14 17:05:55 -0300

    Importing Google Font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,22 @@
+import { Prompt } from 'next/font/google'; 
 import './globals.css';
 import { Aside } from '@/components/Aside';
-
 
 export const metadata = {
   title: "Code connect",
   description: "Uma rede social para devs!",
 };
 
+const prompt =  Prompt({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap'
+});
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className={prompt.className}>
       <body>
         <div className='app-container'>
           <Aside />
