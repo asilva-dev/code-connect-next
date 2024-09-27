89a2d4ff0ac913699e91740e2d05bb6328e70b9a
asilva-dev <amanda.adgti@gmail.com>
2024-09-27 19:00:01 -0300

    Create searchParams

--- src/app/page.js
+++ src/app/page.js
@@ -15,13 +15,16 @@ import Link from "next/link"
     return response.json()
   }
   
-  export default async function Home() {
-    const {data: posts, prev, next} = await getAllPosts(1)
+  export default async function Home({ searchParams }) {
+    const currentPage = searchParams?.page || 1
+    const { data: posts, prev, next } = await getAllPosts(currentPage)
     return (
       <main className={styles.grid}>
-        {posts.map(post =>  <CardPost post={post} />)}
-        {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
-        {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
+        {posts.map(post =>  <CardPost key={post.id} post={post} />)}
+        <div className={styles.links}>
+          {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
+          {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
+        </div>
       </main>
     )
   }
